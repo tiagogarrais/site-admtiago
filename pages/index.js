172676a553e8ba5760f96b85e1e8a974ea2f8058
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2021-02-24 02:00:43 -0300

    Adicionando login com Google
    Adicionando integração de login com o Google

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,53 @@
 import MenuSuperiorGeral from '../components/MenuSuperiorGeral';
 import MenuAtalhosGeral from '../components/MenuAtalhosGeral';
 import RodapeSite from '../components/RodapeSite';
+import GoogleLogin from 'react-google-login';
+import React, { useState } from "react";
 
 
 function Index() {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [profilePic, setProfilePic] = useState()
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+
+  const responseGoogle = (response) => {
+    console.log(response)
+    const { profileObj: { name, email, imageUrl } } = response
+    setName(name)
+    setEmail(email)
+    setProfilePic(imageUrl)
+    setIsLoggedIn(true)
+  }
+
+  const failure = (response) =>{
+    alert('Tente novamente')
+  }
+
+
   return (
-  <div>
-    <MenuSuperiorGeral />
-    <MenuAtalhosGeral />
-    <body>
-      <p>Nesta área, será implementada a autenticação do Google</p>
-    </body>
-    <RodapeSite />
-  </div>
+    <div style={{textAlign:'center'}}>
+      <MenuSuperiorGeral />
+      <MenuAtalhosGeral />
+      <GoogleLogin
+        clientId="752079920699-0ovm5mbbfddb5nrb1skh05nphffehtcs.apps.googleusercontent.com"
+        buttonText="Autenticar com Google"
+        onSuccess={responseGoogle}
+        onFailure={failure}
+        cookiePolicy={'single_host_origin'}
+        style={{textAlign:'center'}}
+      />
+      <hr />
+      {isLoggedIn ? 
+        <div style={{textAlign:'center'}}>
+        <img src={profilePic} alt="Foto do perfil" />
+        <p>Nome: {name}</p>
+        <p>E-mail: {email}</p>
+      </div> : ''}
+      <hr />
+      <RodapeSite />
+    </div>
   );
 }
 export default Index
